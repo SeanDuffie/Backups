be0134e5f45975cf36a8976b114aae572b4db43c
SeanDuffie <duffiesean@gmail.com>
2024-07-26 16:18:17 -0400

    FIX: Greatly simplified schedule

--- schedule.py
+++ schedule.py
@@ -25,45 +25,50 @@ class Scheduler(threading.Timer):
 
         logger.warning("Seconds until First %s Backup: %s", *self.args, (self.tnext - self.tprev).total_seconds())
 
-    def next_time(self, prev: datetime.datetime, interval: datetime.timedelta):
+    def next_time(self, prev: datetime.datetime = None, interval: datetime.timedelta = None):
         """ Calculates the next timestamp that "run" will be active.
 
         Args:
-            prev (datetime.datetime): Last timestamp
-            interval (datetime.timedelta): _description_
+            prev (datetime.datetime): Timestamp of last action.
+            interval (datetime.timedelta): Seconds in between actions.
 
         Returns:
             _type_: _description_
         """
-        if interval > datetime.timedelta(seconds=86399):
-            # If the program is started in the morning between 12AM and 6AM, round next time down
-            if prev.hour < 6:
-                day = prev.day
-            # Otherwise, round next time up
-            else:
-                day = prev.day + interval.days
+        if prev is None:
+            prev = self.tprev
+        if interval is None:
+            interval = self.invl
+        # if interval > datetime.timedelta(seconds=86399):
+        #     # If the program is started in the morning between 12AM and 6AM, round next time down
+        #     if prev.hour < 6:
+        #         day = prev.day
+        #     # Otherwise, round next time up
+        #     else:
+        #         day = prev.day + interval.days
 
-            # Always back up at the next 6AM occurrence.
-            nxt = datetime.datetime(
-                year=prev.year,
-                month=prev.month,
-                day=day,
-                hour=6,
-                minute=0,
-                second=0,
-                microsecond=0
-            )
-        else:
-            # Always back up at the top of the next hour
-            nxt = datetime.datetime(
-                year=prev.year,
-                month=prev.month,
-                day=prev.day + interval.days,
-                hour=(prev.hour + (interval.seconds // 3600)) % 24,
-                minute=0,
-                second=0,
-                microsecond=0
-            )
+        #     # Always back up at the next 6AM occurrence.
+        #     nxt = datetime.datetime(
+        #         year=prev.year,
+        #         month=prev.month,
+        #         day=day,
+        #         hour=6,
+        #         minute=0,
+        #         second=0,
+        #         microsecond=0
+        #     )
+        # else:
+        #     # Always back up at the top of the next hour
+        #     nxt = datetime.datetime(
+        #         year=prev.year,
+        #         month=prev.month,
+        #         day=prev.day + interval.days,
+        #         hour=(prev.hour + (interval.seconds // 3600)) % 24,
+        #         minute=0,
+        #         second=0,
+        #         microsecond=0
+        #     )
+        nxt = prev + interval
         logger.info("Next %s Backup time is at %s (currently %s)", *self.args, nxt, datetime.datetime.now())
         return nxt
 
